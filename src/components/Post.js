201741932947ee45315bf9c13624758239457d26
juanverna <jvergniaud17@gmail.com>
2021-11-08 10:43:47 -0300

    Posts arreglos

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react'
-import { View, Text, Image, StyleSheet, Modal } from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Modal } from 'react-native'
 import { auth, db } from '../firebase/config';
 import firebase from 'firebase';
 
 export default class Post extends Component{
+
     constructor(props){
         super(props);
-        this.state={
+        this.state = {
             liked: false,
             likes: 0,
-            showModal: false
+            showModal: false,
         }
     }
 
     componentDidMount(){
-        if(this.props.dataItem){
-            if(this.props.dataItem.data.likes.length !== 0){
+        if (this.props.dataItem){
+            if (this.props.dataItem.data.likes.length !== 0){
                 this.setState({
                     likes: this.props.dataItem.data.likes.length
                 })
-                if(this.props.dataItem.data.likes.includes(auth.currentUser.email)){
+                if (this.props.dataItem.data.likes.includes(auth.currentUser.email)){
                     this.setState({
                         liked: true
                     })
@@ -30,11 +30,12 @@ export default class Post extends Component{
     }
 
     onLike(){
-        const postUpdate= db.collection('posts').doc(this.props.id)
-        postUpdate.update({
+        const posteoActualizar = db.collection('posts').doc(this.props.dataItem.id)
+        
+        posteoActualizar.update({
             likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
         })
-        .then(()=>{
+        .then(()=> {
             this.setState({
                 liked: true,
                 likes: this.state.likes + 1
@@ -42,12 +43,13 @@ export default class Post extends Component{
         })
     }
 
-    onUnlike(){
-        const postUpdate= db.collection('posts').doc(this.props.dataItem.id)
-        postUpdate.update({
+    onDislike(){
+        const posteoActualizar = db.collection('posts').doc(this.props.dataItem.id)
+        
+        posteoActualizar.update({
             likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
         })
-        .then(()=>{
+        .then(()=> {
             this.setState({
                 liked: false,
                 likes: this.state.likes - 1
@@ -55,57 +57,75 @@ export default class Post extends Component{
         })
     }
 
+    
     showModal(){
+        console.log('Mostrando modal')
         this.setState({
-            showModal: true
+            showModal: true,
         })
     }
-
+    
+    
     closeModal(){
+        console.log('Cerrando modal')
         this.setState({
-            showModal: false
+            showModal: false,
         })
     }
-
+    
     render(){
+
         console.log(this.props.dataItem);
         return(
             <View stlye={styles.container}>
                 <Text>{this.props.dataItem.data.description}</Text>
                 <Text>{this.props.dataItem.data.createdAt}</Text>
                 <Text>{this.props.dataItem.data.owner}</Text>
-                <Text> Liked by {this.state.likes} users </Text>
+                <Text>Likes: {this.state.likes}</Text>
                 {
                     !this.state.liked ?
-                    <TouchableOpacity onPress= {()=> this.onLike()}>
-                        <Text> Like </Text>
+                    <TouchableOpacity onPress = {()=> this.onLike()}>
+                        <Text>
+                            Like
+                        </Text>
                     </TouchableOpacity>
                     :
-                    <TouchableOpacity onPress= {()=> this.onUnlike()}>
-                        <Text> Unlike </Text>
+                    <TouchableOpacity onPress = {()=> this.onDislike()}>
+                        <Text>
+                            Unlike
+                        </Text>
                     </TouchableOpacity>
                 }
-
-                <TouchableOpacity onPress= {()=> this.showModal()}>
-                    <Text> Ver comentarios </Text>
+                <TouchableOpacity onPress={()=>{this.showModal()}}>
+                    <Text>
+                        Ver comentarios
+                    </Text>
                 </TouchableOpacity>
                 {
                     this.state.showModal ?
-                    <Modal visible={this.state.showModal} animationType= 'fade' transparent={false} style={styles.modal}>
-                        <View>
-                            <TouchableOpacity onPress= {()=>this.closeModal()}>
-                                <Text> X </Text>
-                            </TouchableOpacity>
-                            <Text>
-                                Aquí irán los comentarios
-                            </Text>
-                            <Text>
-                                Para agregar comentarios
-                            </Text>
-                        </View>
-                    </Modal>
-                    :
-                    null
+
+                        <Modal 
+                        animationType = "fade"
+                        transparent = {false}
+                        visible = {this.state.showModal}
+                        style = {styles.modal}
+                        >
+                            <View style={styles.modalView}>
+                                {/* Botón de cierre del modal */}
+                                <TouchableOpacity style={styles.closeModal} onPress={()=>{this.closeModal()}}>
+                                        <Text style={styles.modalText} >X</Text>
+                                </TouchableOpacity>
+                                <Text>
+                                    Pondriamos los comments  
+                                </Text>
+                                <Text>
+                                    Posible comment
+                                </Text>
+                            </View>
+
+                        </Modal>
+                        :
+                        null
                 }
             </View>
         )
@@ -113,15 +133,34 @@ export default class Post extends Component{
 }
 
 const styles = StyleSheet.create({
-    image:{
+    image: {
         height: 200,
+    
     },
     container:{
         flex: 1,
         justifyContent: 'center',
         padding: 5,
     },
-    modal:{
-        backgroundColor: 'green'
+    
+    closeModal:{
+        alignSelf: 'flex-end',
+        padding: 10,
+        backgroundColor: '#dc3545',
+        marginTop:2,
+        marginBotom: 10,
+        borderRadius: 4,
+    },
+
+    modalText:{
+        fontWeight: 'bold',
+        color:'#fff',
+    },
+    modalView:{
+        backgroundColor: 'green',
+        borderRadius: 10,
+    },
+    modal: {
+        border: 'none',
     }
 })
