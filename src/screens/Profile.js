b6433878649696638717c90ac996bfd9f7e45afe
juanverna <jvergniaud17@gmail.com>
2021-11-18 08:59:10 -0300

    profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,54 +1,86 @@
-import React, {Component} from 'react';
-import { Text, TextInput, TouchableOpacity, View, StyleSheet, ImageBackground} from 'react-native';
-import { auth, db } from '../firebase/config';
+import React, { Component } from "react";
+import { Text, View, StyleSheet, ImageBackground } from "react-native";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
+import { auth, db } from "../firebase/config";
+import Post from "../components/Post";
 
-export default class CreatePost extends Component {
+export default class Home extends Component {
     constructor(props){
         super(props);
-        this.state= {}
+        this.state = {
+            posts: [],
+        };
     }
-    
-    render(){
-        return(
-            <View style={styles.container}>
-            <ImageBackground source={require('../../assets/bg.png')} style={styles.image}>
-                <Text style={styles.profile}> Username: {auth.currentUser.displayName} </Text>
-                <Text style={styles.profile}> E-Mail: {auth.currentUser.email} </Text>
-                <TouchableOpacity style= {styles.button} onPress={()=> this.props.handleLogout()}>
-                    <Text style= {styles.text}> Logout </Text>
-                </TouchableOpacity>
-            </ImageBackground>
-            </View>
+
+    componentDidMount(){
+        db.collection("posts")
+        .where("owner", "==", auth.currentUser.displayName)
+        .orderBy("createdAt", "desc")
+        .onSnapshot(
+            (docs)=>{
+                let postsAux= [];
+                docs.forEach((doc)=>{
+                    postsAux.push({
+                        id: doc.id,
+                        data: doc.data(),
+                    });
+                });
+                this.setState({
+                    posts: postsAux,
+                })
+            }
         )
     }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <ImageBackground source={require('../../assets/bg.png')} style={styles.bg}>
+                <Text style={styles.text}>Usuario: {auth.currentUser.displayName}</Text>
+                <Text style={styles.text}>E-mail: {auth.currentUser.email}</Text>
+                <Text style={styles.text}>Último ingreso: {auth.currentUser.metadata.lastSignInTime}</Text>
+                <Text style={styles.text}>Posts: {this.state.posts.length}</Text>{" "}
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => this.props.handleLogout()}
+                >
+                    <Text style={styles.text}> Logout </Text>
+                </TouchableOpacity>
+                <FlatList
+                    data={this.state.posts}
+                    keyExtractor={(post)=> post.id.toString()}
+                    renderItem={({ item })=> <Post dataItem={item}></Post>}
+                    style={styles.list}
+                />
+                </ImageBackground>
+            </View>
+        );
+    }
+
 }
 
 const styles= StyleSheet.create({
-    container: {
-        flex: 1
-    },
-
-    button: {
-        width: '40%',
-        backgroundColor: 'salmon',
-        textAlign: 'center',
-        padding: 10,
-    },
-
-    text: {
-        color: 'white',
-        fontSize: 15
-    },
-
-    image: {
+    bg:{
         flex: 1,
         alignItems: 'center',
         width: '100%',
-        height: '100%',
+        height: '100%',      
     },
-
-    profile:{
-        margin: 10,
-        color: 'white'
-    }
+    container: {
+        flex: 1,
+    },
+    list:{
+        width: '50%',
+        margin: 10
+    },
+    button: {
+        width: "100%",
+        borderRadius: 4,
+        backgroundColor: "salmon",
+        padding: 10,
+    },
+    text: {
+        color: "white",
+    },
 })
+
